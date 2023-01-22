09c1d4b49a646df37141d04a9b73c87f0b5c3ea5
Raisei <2718968976@qq.com>
2023-01-22 20:02:31 +0800

    backup

--- VSCode_Python/上机考试/1水仙花.py
+++
@@ -1,7 +0,0 @@
-for i in range(1, 1000):
-    bai = i % 1000 // 100
-    shi = i % 100 // 10
-    ge = i % 10
-    tmp = shi * shi * shi + bai * bai * bai + ge * ge * ge
-    if tmp == i:
-        print(i,"其三位数：", bai, shi, ge)
